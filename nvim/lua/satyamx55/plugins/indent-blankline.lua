4d6cd7b4f9e4e98c466f77af342abc4908a89308
Satyam Kumar <satyamsingh748846@gmail.com>
2025-08-26 23:29:21 +0530

    feat: multiple color indentation , rainbow indentation

--- nvim/lua/satyamx55/plugins/indent-blankline.lua
+++ nvim/lua/satyamx55/plugins/indent-blankline.lua
@@ -12,9 +12,10 @@ return {
       "RainbowViolet",
       "RainbowCyan",
     }
+
     local hooks = require("ibl.hooks")
-    -- create the highlight groups in the highlight setup hook, so they are reset
-    -- every time the colorscheme changes
+
+    -- reset highlights every time colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
       vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
       vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
@@ -25,8 +26,10 @@ return {
       vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
     end)
 
-    vim.g.rainbow_delimiters = { highlight = highlight }
-    require("ibl").setup({ scope = { highlight = highlight } })
+    require("ibl").setup({
+      indent = { highlight = highlight }, -- rainbow indents
+      scope = { highlight = highlight }, -- rainbow scope (optional)
+    })
 
     hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
   end,
